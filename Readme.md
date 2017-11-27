04cd16864ab6c54d7d02a801ef516f92ca9e63c2
Junwei Cen <cenjw@firstshare.cn>
2017-11-27 11:47:00 +0800

    first push

---
+++ Readme.md
@@ -0,0 +1,91 @@
+## AMD2CMD Webpack Plugin
+===========================
+
+This is a [webpack](http://webpack.github.io/) plugin that transform AMD module into CMD module when the library is exposed as an AMD module. Because webpack can not support output library as an CMD module, so this plugin is useful when your library is supposed to run within CMD module loader like seajs.
+
+### Installation
+-----------------
+Install the plugin with npm:
+```shell
+$ npm install amd2cmd-webpack-plugin --save-dev
+```
+
+### Usage
+----------------
+```javascript
+var Amd2CmdWebpackPlugin = require('amd2cmd-webpack-plugin')
+var webpackConfig = {
+    entry: {
+        // ...
+    },
+    externals: {
+        'jquery': '$',
+        // ...
+    },
+    output: {
+        // ...
+        libraryTarget: 'amd'
+    },
+    plugins: [
+        new HtmlWebpackPlugin(),
+        // ...
+    ]
+};
+```
+
+This will make the library exposed as an CMD module:
+```javascript
+define(function(require, cmdExports, cmdModule) {
+    var __WEBPACK_EXTERNAL_MODULE_70__ = require('$');
+    cmdModule.exports = /**** webpack source ****/;
+});
+```
+
+### Configuration
+------------------
+In order to support js concat and ie (version 6-9), `require` function within CMD module is supposed to some typographic conventions, [CMD details](https://github.com/seajs/seajs/issues/426). 
+
+You can do this with `UglifyJsPlugin` to reserve `require` identifiers, then use CMD build tools to generate `id` and `dependencies`.
+```javascript
+var Amd2CmdWebpackPlugin = require('amd2cmd-webpack-plugin')
+var webpackConfig = {
+    // ...
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            mangle: {
+                except: ['require']
+            },
+        })
+    ]
+};
+```
+
+You can also pass `id` configuration options to add `id` property within CMD module, and the plugin will generate `dependencies` autoly.
+```javascript
+var webpackConfig = {
+    // ...
+    entry: {
+        'webpack-plugin': ''
+    },
+    externals: {
+        'jquery': '$',
+        // ...
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            id: '../amd2cmd/'
+        }),
+        // ...
+    ]
+};
+```
+It works:
+```javascript
+define('../amd2cmd/webpack-plugin', ["$"], function(require, cmdExports, cmdModule) {
+    // ...
+});
+```
+
+# License
+
+This project is licensed under [MIT](https://github.com/jantimon/html-webpack-plugin/blob/master/LICENSE).
---
+++ index.js
@@ -0,0 +1,82 @@
+/*
+    Author Junrey Cen <junrey@foxmail.com>
+*/
+
+"use strict";
+
+const ConcatSource = require("webpack-sources").ConcatSource;
+const RawSource = require("webpack-sources").RawSource;
+const Template = require("webpack/lib/Template");
+
+class Amd2CmdWebpackPlugin {
+    constructor(options = {}) {
+        this.id = options.id;
+    }
+
+    apply(compiler) {
+        const name = compiler.options.output.library;
+
+        compiler.plugin("this-compilation", (compilation) => {
+            const mainTemplate = compilation.mainTemplate;
+
+            compilation.templatesPlugin("render-with-entry", (source, chunk, hash) => {
+                const externals = chunk.modules.filter((m) => m.external);
+                const externalsDepsArray = externals.map((m) =>
+                    typeof m.request === "object" ? m.request.amd : m.request
+                );
+                const externalsArguments = externals.map((m) =>
+                    Template.toIdentifier(`__WEBPACK_EXTERNAL_MODULE_${m.id}__`)
+                );
+
+                const externalsRequire = externalsArguments.map((m, i) => `var ${m} = require('${externalsDepsArray[i]}');\n`).join("");
+
+                if (this.id) {
+                    const id = `'${this.id}/${chunk.name}'`;
+                    return new ConcatSource(`
+                    define(${id}, ${JSON.stringify(externalsDepsArray)}, function(require, cmdExports, cmdModule) {\n
+                        ${externalsRequire}
+                        cmdModule.exports = `, source, `
+                    });
+                    `);
+                } else {
+                    return new ConcatSource(`
+                    define(function(require, cmdExports, cmdModule) {\n
+                        ${externalsRequire}
+                        cmdModule.exports = `, source, `
+                    });
+                    `);
+                }
+            });
+
+            compilation.plugin("optimize-chunk-assets", (chunks, callback) => {
+                chunks.forEach((chunk) => {
+                    chunk.files.forEach((file) => {
+                        if (/\.js$/.test(file)) {
+                            const asset = compilation.assets[file];
+                            const input = asset.source();
+
+                            const source = (s => {
+                                    s = s.replace(/define[\s\S]+?define/, 'define');
+                                    s = s.replace(/\}\);\s*\}\);;$/, '\n});');
+                                    return s;
+                            })(input);
+
+                            compilation.assets[file] = new ConcatSource(new RawSource(source));
+                        }
+                    });
+                });
+                callback();
+            });
+
+            mainTemplate.plugin("global-hash-paths", (paths) => {
+                if (name) paths.push(name);
+                return paths;
+            });
+            mainTemplate.plugin("hash", (hash) => {
+                hash.update("exports cmd");
+                hash.update(name);
+            });
+        });
+    }
+}
+module.exports = Amd2CmdWebpackPlugin;
